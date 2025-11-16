4634ecbf12f2735a561b26e243a2f3838179e195
Atharv Naidu <atharvan.naidu@outlook.com>
2025-11-15 22:31:19 -0800

    added goroutine framework

--- query/go.mod
+++ query/go.mod
@@ -3,6 +3,10 @@ module query
 go 1.24.1
 
 require (
+	github.com/bwmarrin/discordgo v0.29.0 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/neo4j/neo4j-go-driver/v5 v5.28.4 // indirect
+	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
+	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
 )
--- query/query.go
+++ query/query.go
@@ -6,9 +6,29 @@ import (
     "github.com/neo4j/neo4j-go-driver/v5/neo4j"
 	"github.com/joho/godotenv"
 	"os"
+	"github.com/bwmarrin/discordgo"
 )
 
-func main() {
+
+//struct that represents a discord reply
+type Interaction struct {
+	User1 string
+	User2 string
+	Message1 string
+	Message2 string
+}
+
+//goroutine that listens for new messages, if message is a reply, send the data as an Interaction  
+func listener(discord discordgo, result chan Interaction) {
+	
+}
+
+func addDatabase() {
+
+}
+
+//connect to neo4j aura db
+func connectNeo4j() {
 	ctx := context.Background()
 	godotenv.Load("../.env")
 	
@@ -25,4 +45,20 @@ func main() {
         panic(err)
     }
     fmt.Println("Connection established.")
+	return ctx, driver
+}
+
+
+//conenect to discord chat websocket API
+func connectDiscord() {
+	discord, err := discordgo.New("Bot " + "authentication token")
+	return discord
+}
+
+
+func main() {
+	ctx, driver = connectNeo4j()
+	discord = connectDiscord()
+
+	go listener(discord)
 }
